b7a50e0ded697e0a8a40e0162b1a25a186cde45f
Carolina Karklis <packer683@gmail.com>
2023-11-20 00:07:14 -0300

    Create api token model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_20_022361) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_030256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_20_022361) do
   create_table "alien_ships", force: :cascade do |t|
     t.integer "max_crew"
     t.integer "abductions_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "api_tokens", force: :cascade do |t|
+    t.boolean "active", default: true
+    t.text "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
