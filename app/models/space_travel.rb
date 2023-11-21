a40c8b9207afb829215d61c5a510c128836f67a9
Carolina Karklis <packer683@gmail.com>
2023-11-21 01:19:03 -0300

    Calculate duration of travel and add validation to model

--- app/models/space_travel.rb
+++ app/models/space_travel.rb
@@ -4,6 +4,12 @@ class SpaceTravel < ApplicationRecord
   belongs_to :planet
   belongs_to :space_vehicle
 
+  validates :start_at, presence: true
+
+  validate :enough_vehicle_fuel
+
+  before_validation :calculate_duration_in_days
+
   enum status: {
     scheduled: 0,
     started: 1,
@@ -34,5 +40,19 @@ class SpaceTravel < ApplicationRecord
     event :fail do
       transitions from: :started, to: :failed
     end
+  end
+
+  private
+
+  def calculate_duration_in_days
+    duration_in_hours = planet.km_from_earth / space_vehicle.km_per_hour
+
+    self.duration_in_days = (duration_in_hours.to_f / 24).ceil
+  end
+
+  def enough_vehicle_fuel
+    return if space_vehicle.vehicleable_type == 'AlienShip' || space_vehicle.vehicleable.fuel_days >= duration_in_days
+
+    errors.add(:space_vehicle, "vehicle hasn't enough fuel to schedule travel")
   end
 end
